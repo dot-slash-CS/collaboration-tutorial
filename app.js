a0b8b993e720e5dc2cda6a9c42f5bcb8561f9768
Pirjot <apirjot@gmail.com>
2021-10-08 17:56:01 -0700

    New

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.json());
+
+app.post('/add-class', (req, res) => {
+    //...
+    // These lines would add the class and do server stuff
+    //...
+    res.send({
+        body: req.body.class,
+        message: "Your class was added to the database."
+    });
+});
+
+app.post('/add-class', (req, res) => {
+    //...
+    // These lines would add the class and do server stuff
+    //...
+    res.send({
+        body: req.body.class,
+        message: "Your class was added to the database."
+    });
+});
+app.post('/add-class', (req, res) => {
+    //...
+    // These lines would add the class and do server stuff
+    //...
+    res.send({
+        body: req.body.class,
+        message: "Your class was added to the database."
+    });
+});
+app.post('/add-class', (req, res) => {
+    //...
+    // These lines would add the class and do server stuff
+    //...
+    res.send({
+        body: req.body.class,
+        message: "Your class was added to the database."
+    });
+});
+app.post('/add-class', (req, res) => {
+    //...
+    // These lines would add the class and do server stuff
+    //...
+    res.send({
+        body: req.body.class,
+        message: "Your class was added to the database."
+    });
+});
+app.post('/add-class', (req, res) => {
+    //...
+    // These lines would add the class and do server stuff
+    //...
+    res.send({
+        body: req.body.class,
+        message: "Your class was added to the database."
+    });
+});
+
+app.listen(1515, () => console.log("Listening"));
---
+++ public/main.js
@@ -0,0 +1,36 @@
+/* main.js
+ * @authors: Pirjot Atwal
+*/
+
+//Put some code here to run!
+
+/**
+ * Prints Hello to the console with the passed in name.
+ * 
+ * @param {String} name 
+ * @author Pirjot
+ */
+function printHello(name) {
+    console.log("Hello " + name + "!");
+}
+
+printHello("User!");
+
+function attachAction() {
+    document.getElementById("myButton").addEventListener("click", async (evt) => {
+        evt.preventDefault();
+        //Send request to add class to server
+        let options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({class: "Math-101A"})
+        }
+        let response = await fetch('/add-class', options);
+        console.log(await response.json());
+    });
+}
+
+// document.addEventListener("DOMContentLoaded", attachAction);
+attachAction();
